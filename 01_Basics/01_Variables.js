6d9281261e97efbc6b18a441e11adbc3be0e1f42
Shaikh Zaid Azhar <shaikhzaidazhar@gmail.com>
2025-09-24 11:38:30 +0000

    Variables(const, let, var(not to use))

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,10 @@
+const accountId = "123457"
+let accountEmail = "zaid@gmail.com"
+var accountPassword = "9867"
+accountCity = "Mumbai"
+let accountState;
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
