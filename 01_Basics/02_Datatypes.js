97203a2aa66fea97b6a72f69318bdf1276cbcc8f
Shaikh Zaid Azhar <shaikhzaidazhar@gmail.com>
2025-09-25 14:23:07 +0000

    DataTypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+// DataTypes
+
+let name = "Zaid" 
+let age = 18
+let bigInt = 46941361649616161619191619161649494
+let state = "Maharaashtra"
+
+
+// List of DataTYpes
+//number => 2 to the power of 53
+// bigInt ... use for Higher numbers e.g user of Insta/ reddit etc
+//  string = ""
+// boolean = true/false ... only 2 types are there of boolean data type
+// null()
+// symbol => uniqueness ... useed maninly in React.js
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof bigInt);
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
