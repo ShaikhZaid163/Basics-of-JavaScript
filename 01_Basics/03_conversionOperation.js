1cc3cf7c5fbbd8e85f90910f40de5083315b1462
Shaikh Zaid Azhar <shaikhzaidazhar@gmail.com>
2025-09-25 20:42:14 +0000

    Conversion of DataTypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = undefined
+
+// console.log(typeof (score));
+// console.log(typeof score);
+//console.log("Value number ofscore is:" +score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log("Type of this is :" +valueInNumber); // NaN
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+// null => 0
+// undefined => NaN
+
+//Boolean Conversion
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log("This is: " + booleanIsLoggedIn)
+// 1 => true , 0 => false
+// "" => false , "abc" => true
+// null => false
+// undefined => false
+// " " => true
+
+//Just by changing "String" or "Boolean" etc we can can convert the data types
+
+let someNumber = 34
+let stringNumber = String(someNumber)
+console.log("This is: " + stringNumber)
+console.log(typeof stringNumber)
