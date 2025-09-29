586003c8b18f552f2ac07123412bd2f4714a6bcf
Shaikh Zaid Azhar <shaikhzaidazhar@gmail.com>
2025-09-29 19:28:57 +0000

    My quick change

---
+++ 01_Basics/04_Comparisons.js
@@ -0,0 +1,15 @@
+// console.log(null > 0); // false 
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+
+// &************ There is 2 different ans becoz ("==" , "><") Acts differnt & ("<==", ">==") these Acts different
+// becoz less than or greater than equal to acts as a "0" but not a NULL or NAN
+// Thats why line no. 1 is FALSE and line no. 2 is TRUE
+
+
+// console.log(undefined > 0); // false 
+// console.log(undefined == 0); // false
+// console.log(undefined <= 0); // false
+
+console.log("2" === 2); //False 
+// Bcoz === strictly checks value and it also checks the Datatype
