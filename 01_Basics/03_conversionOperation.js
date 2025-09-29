96b6ec3ee7eceb10b14fdf8d16623335793faad9
Shaikh Zaid Azhar <shaikhzaidazhar@gmail.com>
2025-09-29 19:29:29 +0000

    My quick change

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -5,8 +5,8 @@ let score = undefined
 //console.log("Value number ofscore is:" +score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log("Type of this is :" +valueInNumber); // NaN
+// console.log(typeof valueInNumber)
+// console.log("Type of this is :" +valueInNumber); // NaN
 // "33" => 33
 // "33abc" => NaN
 // true => 1 , false => 0
@@ -16,7 +16,7 @@ console.log("Type of this is :" +valueInNumber); // NaN
 //Boolean Conversion
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log("This is: " + booleanIsLoggedIn)
+// console.log("This is: " + booleanIsLoggedIn)
 // 1 => true , 0 => false
 // "" => false , "abc" => true
 // null => false
@@ -27,5 +27,15 @@ console.log("This is: " + booleanIsLoggedIn)
 
 let someNumber = 34
 let stringNumber = String(someNumber)
-console.log("This is: " + stringNumber)
-console.log(typeof stringNumber)
+// console.log("This is: " + stringNumber)
+// console.log(typeof stringNumber)
+
+//&***************************************************************************************&
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+//&********************** This thing depends on prefixs and postfixs**********************&
+
+
+
