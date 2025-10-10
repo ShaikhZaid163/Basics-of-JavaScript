bf84a636baca904daf5e7e8a18b5956863121234
Shaikh Zaid Azhar <shaikhzaidazhar@gmail.com>
2025-10-10 13:16:58 +0000

    strings

---
+++ 01_Basics/06_Strings.js
@@ -0,0 +1,19 @@
+const name = "Zaidd"
+const repoCount = 50
+
+// This is the old method of concatenation 
+// console.log(name + repoCount + "how are you"); 
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+// Another way to declare a string
+const stringName = new String ("Zaid")
+
+console.log(typeof stringName)
+console.log(stringName.length);
+console.log(stringName.toUpperCase);
+console.log(stringName.toLowerCase);
+console.log(stringName.split('-'));
+
+
+
+
