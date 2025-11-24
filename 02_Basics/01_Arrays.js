94e004091cdb0998b0cf4948b706a6bc49812ebd
Shaikh Zaid Azhar <175631198+ShaikhZaid163@users.noreply.github.com>
2025-11-24 13:06:51 +0000

    ARRAYS

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,27 @@
+let myarr = [1, 2, 3, 4, 5, ]; // Can name anything to array 
+//console.log(myarr); // JS are Resizable and can hold multiple data types 
+
+/* ************************************************************************************************************************ */
+/* types we can create a ARRAY */
+
+const newArr = new Array ();
+const myarr2 = [7,8,9,10];
+const heros = ['shaktiman', 'naagraj', 'doga', 'batman'];
+//console.log(heros[1]);
+
+
+
+
+/* **********************************ARRAY METHODS*********************************************************************** */
+
+/* myarr2.push(13);
+myarr2.push(11);
+myarr2.push(12); */
+
+//myarr.unshift(12);
+myarr.unshift();
+myarr.unshift();
+console.log(myarr);
+
+
+
